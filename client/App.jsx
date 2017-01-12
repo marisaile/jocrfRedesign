cf9fe802d7f5c4da7613a03f24b09483121a1d76
Lisa Cook <lcook317@gmail.com>
2017-01-12 14:08:39 -0700

    launching server

--- client/App.jsx
+++ client/App.jsx
@@ -2,55 +2,19 @@
 import $ from 'jquery';
 import 'styles/main.scss';
 
-import mainPage from 'pages/mainPage';
-import writingPrompts from 'pages/writingPrompts';
-import jocPage from 'pages/jocPage';
-import TodoListView from 'pages/todoReact/todoListView';
-import funnySquares from 'pages/funnySquares';
-import photoSearch from 'pages/photoSearch';
-import game from 'pages/breakout';
-import scattergories from 'pages/scattergoriesGame';
-import d3aptitude from 'pages/D3aptitudeResult';
-import sampleTests from 'pages/jocSampleTests';
-import d3 from 'pages/d3';
+import tweezer from 'pages/timerTD';
+import observation from 'pages/timerObs';
 
 $(function() {
-
-  mainPage.init();
 
   var url = window.location.pathname;
 
   switch (url) {
-    
-    case '/pages/writingPrompts.html':
-      writingPrompts.init();
-    break; 
-    case '/pages/breakout.html':
-      game.init();
+    case '/pages/tweezerTimer.html':
+      tweezer.init();
     break;
-    case '/pages/jocPage.html':
-      jocPage.init();
-    break;
-    case '/pages/todo.html':
-      var todoListView = new TodoListView(); 
-    break;
-    case '/pages/funnySquares.html':
-      funnySquares.init();
-    break;
-    case '/pages/photoSearch.html':
-      photoSearch.init(); 
-    break;
-    case '/pages/jocSampleTests.html':
-      sampleTests.init();
-    break;
-    case '/pages/scattergoriesGame.html':
-      scattergories.init();
-    break;
-    case '/pages/d3aptitudeResult.html':
-      d3aptitude.init();
-    break;
-    case '/pages/d3.html': 
-      d3.init();
+    case '/pages/observationTimer.html':
+      observation.init();
     break;
     default: break;
   }
